764f7aa4ab0bc7258968fc31c6cf649df04117de
LucasTF <lucas.htferreira@outlook.com>
2023-07-26 18:55:59 -0300

    Added MongoDB connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
-
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db";
 
 import products from "./data/products";
 
 const port = process.env.PORT || 5000;
+
+connectDB();
+
 const app = express();
 
 app.get("/", (req, res) => {
